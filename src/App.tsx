dd5b6f1de8987f67444d39ee9ce2a7ce6a89e0b9
Goat95 <hjh9703@naver.com>
2023-02-27 05:42:26 +0900

    Home Page Finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import Navbar from "@/scenes/navbar";
+import Home from "@/scenes/home";
+import Benefits from "@/scenes/benefits";
 import { useEffect, useState } from "react";
 import { SelectedPage } from "@/shared/types";
 
@@ -30,6 +32,7 @@ function App() {
         selectedPage={selectedPage}
         setSelectedPage={setSelectedPage}
       />
+      <Home setSelectedPage={setSelectedPage} />
     </div>
   );
 }
